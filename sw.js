55a59b583e1519e2107af0b5bfcb9fab44537398
thegioiqr2026-ux <thegioiqr2026@gmail.com>
2026-01-26 05:12:46 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'thegioiqr-v1.4';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/app.js',
+    '/pages.js',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// Cài đặt Service Worker và lưu Cache
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+// Kích hoạt và xóa Cache cũ
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+        })
+    );
+});
+
+// Phản hồi yêu cầu khi Offline
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => {
+            return res || fetch(e.request);
+        })
+    );
+});
